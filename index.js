e7800819940c7e557650d2e84fa0916187b1ed26
Necip <ncptzgn@gmail.com>
2021-10-15 17:14:32 +0300

    Express.js routing

--- index.js
+++ index.js
@@ -19,72 +19,19 @@
 //Express.js
 const cors = require("cors");
 const express = require("express");
-let data = require("./data.js");
+const aktorlerRouter = require("./routers/aktorlerRouter")
 
 const server = express();
 server.use(express.json());
+server.use("/aktorler",aktorlerRouter)
 
 server.use(cors({ origin: true }));
 
+
 server.get("/", (req, res) => {
-  res.send("Express'ten merhaba...cd");
-});
+    res.send("Express'ten merhaba...");
+  });
 
-server.get("/aktorler", (req, res) => {
-  res.status(200).json(data);
-});
-
-let next_id = 4;
-
-server.post("/aktorler", (req, res) => {
-  console.log("req.body", req.body);
-  let yeni_aktor = req.body;
-  yeni_aktor.id = next_id;
-  next_id++;
-  data.push(yeni_aktor);
-  res.status(201).json(yeni_aktor);
-});
-
-server.delete("/aktorler/:id", (req, res) => {
-  const silinecek_id = req.params.id;
-  const silinecek_aktor = data.find(
-    (aktor) => aktor.id === Number(silinecek_id)
-  );
-
-  if(silinecek_aktor){
-    data = data.filter(aktor => aktor.id !== Number(silinecek_id));
-    res.status(204).end();
-  }else{
-      res.status(404).json({errorMessage:"Silmeye calıstıgınız aktor sıstemde yok."})
-  }
-});
-
-
-server.put("/aktorler/:id",(req,res)=>{
-    console.log(req.body)
-    const id = req.params.id;
-    const body = req.body
-    const current_aktor = data.find(aktor=>aktor.id === parseInt(id));
-    console.log(current_aktor)
-    if(current_aktor){
-        data[id-1] = body
-        res.status(204).end();
-        }      
-    else{
-        res.status(404).json({errorMessage:"Degistirmek istediginiz parametreler hatalı."})
-    }
-})
-
-server.get("/aktorler/:id", (req, res) => {
-  console.log("req.body", req.body);
-  const { id } = req.params;
-  const aktor = data.find((aktor) => aktor.id === parseInt(id));
-  if (aktor) {
-    res.status(200).json(aktor);
-  } else {
-    res.status(404).send("Aradıgınız aktor bulunamadı...");
-  }
-});
 
 server.listen(5000, () => {
   console.log("http://localhost:5000 adresine gelen istekler dinleniyor...");
---
+++ routers/aktorlerRouter.js
@@ -0,0 +1,65 @@
+const router = require("express").Router();
+let data = require("../data")
+
+const cors = require("cors");
+router.use(cors({ origin: true }));
+  
+  router.get("/", (req, res) => {
+    res.status(200).json(data);
+  });
+  
+  let next_id = 4;
+  
+  router.post("/", (req, res) => {
+    console.log("req.body", req.body);
+    let yeni_aktor = req.body;
+    yeni_aktor.id = next_id;
+    next_id++;
+    data.push(yeni_aktor);
+    res.status(201).json(yeni_aktor);
+  });
+  
+  router.delete("/:id", (req, res) => {
+    const silinecek_id = req.params.id;
+    const silinecek_aktor = data.find(
+      (aktor) => aktor.id === Number(silinecek_id)
+    );
+  
+    if(silinecek_aktor){
+      data = data.filter(aktor => aktor.id !== Number(silinecek_id));
+      res.status(204).end();
+    }else{
+        res.status(404).json({errorMessage:"Silmeye calıstıgınız aktor sıstemde yok."})
+    }
+  });
+  
+  
+  router.put("/:id",(req,res)=>{
+      console.log(req.body)
+      const id = req.params.id;
+      const body = req.body
+      const current_aktor = data.find(aktor=>aktor.id === parseInt(id));
+      console.log(current_aktor)
+      if(current_aktor){
+          data[id-1] = body
+          res.status(204).end();
+          }      
+      else{
+          res.status(404).json({errorMessage:"Degistirmek istediginiz parametreler hatalı."})
+      }
+  })
+  
+  router.get("/:id", (req, res) => {
+    console.log("req.body", req.body);
+    const { id } = req.params;
+    const aktor = data.find((aktor) => aktor.id === parseInt(id));
+    if (aktor) {
+      res.status(200).json(aktor);
+    } else {
+      res.status(404).send("Aradıgınız aktor bulunamadı...");
+    }
+  });
+
+
+
+module.exports = router;
